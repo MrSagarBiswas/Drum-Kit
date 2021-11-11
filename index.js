8b1542ae0c567ebbafc9ff370b3caf4404cf325a
MrSagarBiswas <biswassagar927@gmail.com>
2021-11-12 00:45:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var btn = document.querySelectorAll("button");
+
+for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function () {
+        makeSound(this.innerHTML);
+    }
+    )
+}
+
+document.addEventListener("keypress", function (pressed) {
+    makeSound(pressed.key);
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "s":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "d":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+        case "k":
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case "l":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        default:
+            console.log(this.innerText);
+    }
+}
